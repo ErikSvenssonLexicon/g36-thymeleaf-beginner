1007102220e3ca1fe9fe8cbd22b13b564f3e0883
Erik Svensson <eosvensson@gmail.com>
2021-08-10 14:16:03 +0200

    Finished CRUD

--- src/main/java/se/lexicon/g36thymeleafbeginner/controller/ProductController.java
+++ src/main/java/se/lexicon/g36thymeleafbeginner/controller/ProductController.java
@@ -64,4 +64,49 @@ public class ProductController {
 
         return "productView";
     }
+
+    @GetMapping("/products/{id}/update")
+    public String getUpdateForm(@PathVariable String id, Model model){
+        Optional<Product> op = productList.stream()
+                .filter(p -> p.getId().equals(id))
+                .findFirst();
+
+        if(op.isPresent()){
+            model.addAttribute("product", op.get());
+        }else {
+            return "redirect:/products";
+        }
+
+        return "product-form-update";
+    }
+
+    @PostMapping("/products/{id}/update/process")
+    public String update(
+            @PathVariable String id,
+            @RequestParam(name = "name") String name,
+            @RequestParam(name = "price") BigDecimal price,
+            @RequestParam(name = "description") String description
+            ){
+        Optional<Product> op = productList.stream()
+                .filter(p -> p.getId().equals(id))
+                .findFirst();
+
+        if(op.isPresent()){
+            Product product = op.get();
+            product.setName(name.trim());
+            product.setPrice(price);
+            product.setDescription(description.trim());
+            return "redirect:/products/"+id;
+        }else {
+            return "redirect:/products";
+        }
+    }
+
+    @GetMapping("/products/{id}/delete")
+    public String delete(@PathVariable String id){
+        productList.removeIf(product -> product.getId().equals(id));
+        return "redirect:/products";
+    }
+
+
 }
