4eb449bcf811490dc198b0e6e22ab92d607ee993
Erik Svensson <eosvensson@gmail.com>
2021-08-10 10:58:51 +0200

    Initial commit

---
+++ src/main/java/se/lexicon/g36thymeleafbeginner/G36ThymeleafBeginnerApplication.java
@@ -0,0 +1,13 @@
+package se.lexicon.g36thymeleafbeginner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class G36ThymeleafBeginnerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(G36ThymeleafBeginnerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/se/lexicon/g36thymeleafbeginner/G36ThymeleafBeginnerApplicationTests.java
@@ -0,0 +1,13 @@
+package se.lexicon.g36thymeleafbeginner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class G36ThymeleafBeginnerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
